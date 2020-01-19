7822b7b69454a4ffa548408aa0c2e16a5f579a6f
Andrew LI <zhipenglee1994@gmail.com>
2020-01-20 01:10:51 +1030

    Court listing func finished

--- mycourt/src/CourtList.js
+++ mycourt/src/CourtList.js
@@ -7,7 +7,7 @@ class CourtList extends Component {
     super(props);
     this.state = {
       inputValus:'',
-      list:["1","2","3"]
+      list:[]
     }
   }
 
@@ -15,12 +15,12 @@ class CourtList extends Component {
     return (
       <Fragment>
         <div className="courtList">Court List</div>
-        <input type="text" ></input>
-        <button onClick = {this.addToList}>Add</button>
+        <input type="text" onChange={this.handleUserInput.bind(this)} value = {this.state.inputValus}></input>
+        <button onClick = {this.addToList.bind(this)}>Add</button>
         <ul>
           {
             this.state.list.map((item,index)=>{
-                return <li>{item}</li>
+                return <li key={index} onClick ={this.handleDeletItem.bind(this,index)}>{item}</li>
               })
           }
         </ul>
@@ -29,6 +29,24 @@ class CourtList extends Component {
   }
 
   addToList(){
+    this.setState({
+      list:[...this.state.list,this.state.inputValus],
+      inputValus : ""
+    })
+
+  }
+
+  handleUserInput(e){ 
+    this.setState({inputValus:e.target.value})
+  }
+
+
+  handleDeletItem(index){
+    const list = [...this.state.list];
+    list.splice(index,1);
+    this.setState({
+      list:[...list]
+    })
     
   }
 }
